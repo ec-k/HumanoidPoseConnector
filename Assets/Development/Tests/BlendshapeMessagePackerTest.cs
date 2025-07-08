4e8014ac731e8cb40b6152823c2bf51cffe573db
EC-K <elxone.19jt@gmail.com>
2025-07-08 11:48:37 +0900

    wip(test): writing blendshapes message packer test

---
+++ Assets/Development/Tests/BlendshapeMessagePackerTest.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace HumanoidPoseConnector.Tests {
+    public class BlendshapeMessagePackerTest
+    {
+        [UnityTest]
+        public IEnumerator VRoidDefaultBlendshapeGetingTest()
+        {
+            var avatarPrefab = Resources.Load<GameObject>("Avatars/DefauldBlendshapeAvatar/Nico");
+            Assert.IsNotNull(avatarPrefab, "Avatar is not found.");
+            var avatar = GameObject.Instantiate(avatarPrefab);
+            yield return null;
+
+            var faceObj = avatar.transform.Find("Face");
+            var skm = faceObj.GetComponent<SkinnedMeshRenderer>();
+
+            var packer = new BlendshapeMessagePacker();
+        }
+
+        [UnityTest]
+        public IEnumerator PerfectSyncBlendshapeGetingTest()
+        {
+            var avatarPrefab = Resources.Load<GameObject>("Avatars/PerfectSyncAvatar/violet_perfectSync");
+            Assert.IsNotNull(avatarPrefab, "Avatar is not found.");
+            var avatar = GameObject.Instantiate(avatarPrefab);
+            yield return null;
+
+            var faceObj = avatar.transform.Find("Face");
+            var skm = faceObj.GetComponent<SkinnedMeshRenderer>();
+
+            var packer = new BlendshapeMessagePacker();
+        }
+    }
+}
