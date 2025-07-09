4d7c1d10b813046205facbb9e39c54dc082bbc40
EC-K <elxone.19jt@gmail.com>
2025-07-09 16:08:40 +0900

    feat: update server and sender components to conform to VMC protocol

--- Assets/HumanoidPoseConnector/Runtime/Apps/HumanoidPoseReceiver.cs
+++ Assets/HumanoidPoseConnector/Runtime/Apps/HumanoidPoseReceiver.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-using uOSC;
 using UnityEngine;
+using uOSC;
 
 namespace HumanoidPoseConnector
 {
@@ -9,23 +9,52 @@ namespace HumanoidPoseConnector
     public class PoseReceiver : MonoBehaviour
     {
         uOscServer _server;
+        public Dictionary<HumanBodyBones, (Vector3, Quaternion)> PoseResults { get; private set; }
+        public Dictionary<string, float> BlendshapeResults { get; private set; }
         public float LastReceivedTime { get; private set; }
+        public bool IsAvailable { get; private set; }
 
-        public Dictionary<HumanBodyBones, Quaternion> Results { get; set; }
+        public event Action OnBlendshapeApplyRequested;
+
+        const string AvailabilityMessageAddress = "/VMC/Ext/OK";
+        const string BoneTransformMessageAddress = "/VMC/Ext/Bone/Pos";
+        const string BlendshapeWeightMessageAddress = "/VMC/Ext/Blend/Val";
+        const string BlendshapeApplyingMessageAddress = "/VMC/Ext/Blend/Apply";
+        const string TimeMessageAddress = "/VMC/Ext/T";
 
         void Start()
         {
-            Results = new();
+            PoseResults = new();
+            BlendshapeResults = new();
             _server = GetComponent<uOscServer>();
             _server.onDataReceived.AddListener(OnDataReceived);
         }
 
         void OnDataReceived(Message message)
         {
-            var boneKey = (HumanBodyBones)Enum.Parse(typeof(HumanBodyBones), message.address);
-            var rotation = new Quaternion((float)message.values[0], (float)message.values[1], (float)message.values[2], (float)message.values[3]);
-            Results[boneKey] = rotation;
-            LastReceivedTime = Time.time;
+            var address = message.address;
+            switch (address)
+            {
+                case AvailabilityMessageAddress:
+                    IsAvailable = (int)message.values[0] == 1;
+                    break;
+                case BoneTransformMessageAddress:
+                    var bone = MessageParser.ParseBoneTransformMessage(message);
+                    PoseResults[bone.boneKey] = (bone.position, bone.rotation);
+                    break;
+                case BlendshapeWeightMessageAddress:
+                    var parameter = MessageParser.ParseBlendshapeMessage(message);
+                    BlendshapeResults[parameter.key] = parameter.weight;
+                    break;
+                case BlendshapeApplyingMessageAddress:
+                    OnBlendshapeApplyRequested?.Invoke();
+                    break;
+                case TimeMessageAddress:
+                    LastReceivedTime = (float)message.values[0];
+                    break;
+                default:
+                    throw new ArgumentException($"Unknown message address received: {address}", nameof(message.address));
+            }
         }
     }
 }
--- Assets/HumanoidPoseConnector/Runtime/Apps/HumanoidPoseSender.cs
+++ Assets/HumanoidPoseConnector/Runtime/Apps/HumanoidPoseSender.cs
@@ -7,127 +7,63 @@ namespace HumanoidPoseConnector
     public class PoseSender : MonoBehaviour
     {
         uOscClient _client;
-        [SerializeField] Animator _avatarAnimator;
+        [SerializeField] GameObject _avatar;
         [SerializeField] float _sendingRate = 60f;
         [SerializeField] bool _sendPose = true;
         [SerializeField] bool _sendHand = true;
         [SerializeField] bool _sendFacialBlendshapes = true;
+        [SerializeField] bool _isPerfaceSync = false;
+
+        Animator _animator;
+        SkinnedMeshRenderer _faceMeshRenderer;
+        PoseMessagePacker _poseMsgPacker;
+        BlendshapeMessagePacker _blendshapeMsgPacker;
+        VMCMessagePacker _vmcMsgPacker;
 
         public bool IsActive { get; set; }
-            
-        float _timer = 0f;
+
+        float _throttleTimer = 0f;
         float _sendingInterval => 1 / _sendingRate;
 
         void Start()
         {
             _client = GetComponent<uOscClient>();
+            _vmcMsgPacker = new();
+            _poseMsgPacker = new();
+            _blendshapeMsgPacker = new();
+
+            _animator = _avatar.GetComponent<Animator>();
+            if (_animator is null)
+                Debug.LogError("Avatar animator is not found.");
+            _faceMeshRenderer = _avatar.transform.Find("Face").GetComponent<SkinnedMeshRenderer>();
+            if (_faceMeshRenderer is null)
+                Debug.LogError("Avatar's face SkinnedMeshRenderer is not found.");
         }
 
         void Update()
         {
-            if (_avatarAnimator is null || !IsActive) 
-                return;
-
-            _timer += Time.deltaTime;
-
-            if(_sendingInterval <= _timer)
+            _throttleTimer += Time.deltaTime;
+            if(_sendingInterval <= _throttleTimer)
             {
                 SendAvatarPoseMessage();
-                _timer = 0f;
+                _throttleTimer = 0f;
             }
         }
 
         void SendAvatarPoseMessage()
         {
-            var msg = new Bundle(Timestamp.Now);
-            if (_sendPose) msg.Add(SetPoseMessage());
-            if(_sendHand) msg.Add(SetHandMessage());
-            if (_sendFacialBlendshapes) msg.Add(SetBlendshapes());
-            _client.Send(msg);
-        }
+            var avatarMsg = new Bundle(Timestamp.Now);
 
-        Bundle SetPoseMessage()
-        {
-            var msg = new Bundle();
-            msg.Add(SetRotationMessage(HumanBodyBones.Hips));
-            msg.Add(SetRotationMessage(HumanBodyBones.Spine));
-            msg.Add(SetRotationMessage(HumanBodyBones.Chest));
-            msg.Add(SetRotationMessage(HumanBodyBones.UpperChest));
-            msg.Add(SetRotationMessage(HumanBodyBones.Neck));
-            msg.Add(SetRotationMessage(HumanBodyBones.Head));
-            msg.Add(SetRotationMessage(HumanBodyBones.Jaw));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftEye));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightEye));
+            if (_sendPose) avatarMsg.Add(_poseMsgPacker.GeneratePoseMessageBundle(_animator));
+            if(_sendHand) avatarMsg.Add(_poseMsgPacker.GenerateHandMessageBundle(_animator));
+            if (_sendFacialBlendshapes)
+                if (_isPerfaceSync)
+                    avatarMsg.Add(_blendshapeMsgPacker.BundlePerfectSyncBlendshape(_faceMeshRenderer));
+                else
+                    avatarMsg.Add(_blendshapeMsgPacker.BundleVRMDefaultBlendshape(_faceMeshRenderer));
 
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftShoulder));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftUpperArm));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftLowerArm));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftHand));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftUpperLeg));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftLowerLeg));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftFoot));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftToes));
-
-            msg.Add(SetRotationMessage(HumanBodyBones.RightShoulder));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightUpperArm));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightLowerArm));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightHand));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightUpperLeg));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightLowerLeg));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightFoot));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightToes));
-
-            return msg;
-        }
-
-        Bundle SetHandMessage()
-        {
-            var msg = new Bundle();
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftThumbProximal));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftThumbIntermediate));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftThumbDistal));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftIndexProximal));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftIndexIntermediate));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftIndexDistal));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftMiddleProximal));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftMiddleIntermediate));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftMiddleDistal));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftRingProximal));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftRingIntermediate));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftRingDistal));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftLittleProximal));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftLittleIntermediate));
-            msg.Add(SetRotationMessage(HumanBodyBones.LeftLittleDistal));
-
-            msg.Add(SetRotationMessage(HumanBodyBones.RightThumbProximal));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightThumbIntermediate));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightThumbDistal));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightIndexProximal));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightIndexIntermediate));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightIndexDistal));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightMiddleProximal));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightMiddleIntermediate));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightMiddleDistal));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightRingProximal));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightRingIntermediate));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightRingDistal));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightLittleProximal));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightLittleIntermediate));
-            msg.Add(SetRotationMessage(HumanBodyBones.RightLittleDistal));
-            return msg;
-        }
-
-        // TODO: Implement this.
-        Bundle SetBlendshapes()
-        {
-            var msg = new Bundle();
-            return msg;
-        }
-
-        Message SetRotationMessage(HumanBodyBones humanBodyBones)
-        {
-            var boneRotation = _avatarAnimator.GetBoneTransform(humanBodyBones).localRotation;
-            return new Message(humanBodyBones.ToString(), boneRotation.x, boneRotation.y, boneRotation.z, boneRotation.w);
+            var vmcMsg = _vmcMsgPacker.GenerateVMCMessage(avatarMsg);
+            _client.Send(vmcMsg);
         }
     }
 }
