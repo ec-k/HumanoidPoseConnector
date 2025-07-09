fa30bdbe00a47320049040cb1a3727b92a8c60f7
EC-K <elxone.19jt@gmail.com>
2025-07-09 15:25:03 +0900

    feat: allow VMCMessagePacker configurable

--- Assets/HumanoidPoseConnector/Runtime/Common/VMCMessagePacker.cs
+++ Assets/HumanoidPoseConnector/Runtime/Common/VMCMessagePacker.cs
@@ -10,39 +10,20 @@ namespace HumanoidPoseConnector
 
         Animator _avatarAnimator;
         SkinnedMeshRenderer _faceSkinMesh;
-        PoseMessagePacker _poseMessagePacker;
-        BlendshapeMessagePacker _blendshapeMessagePacker;
 
-        bool _isPerfectSyncEnabled;
-
-        public VMCMessagePacker(GameObject avatar, bool isPerfectSyncEnabled = false)
-        {
-            _avatarAnimator = avatar.GetComponent<Animator>();
-            if (_avatarAnimator is null)
-                Debug.LogError("Avatar animator is not found.");
-            _faceSkinMesh = avatar.transform.Find("Face").GetComponent<SkinnedMeshRenderer>();
-            if (_avatarAnimator is null)
-                Debug.LogError("Avatar's face SkinnedMeshRenderer is not found.");
-
-            _poseMessagePacker = new PoseMessagePacker();
-            _blendshapeMessagePacker = new BlendshapeMessagePacker();
-            _isPerfectSyncEnabled = isPerfectSyncEnabled;
+        public VMCMessagePacker()
+        { 
         }
 
-        public Bundle GenerateVMCMessage()
+        public Bundle GenerateVMCMessage(Bundle mainMessageBundle)
         {
             var bundle = new Bundle();
-
-            var poseMsg = _poseMessagePacker.BundleHumanoidBones(_avatarAnimator);
-            var blendshapeMsg = _isPerfectSyncEnabled
-                ? _blendshapeMessagePacker.BundleVRMDefaultBlendshape(_faceSkinMesh)
-                : _blendshapeMessagePacker.BundlePerfectSyncBlendshape(_faceSkinMesh);
+         
             var availability = (_avatarAnimator is not null) && (_faceSkinMesh is not null);
             var availabilityMsg = GenerateAvailabilityMessage(availability);
             var timeMsg = GenerateTimeMessage(Time.time);
 
-            bundle.Add(poseMsg);
-            bundle.Add(blendshapeMsg);
+            bundle.Add(mainMessageBundle);
             bundle.Add(availabilityMsg);
             bundle.Add(timeMsg);
 
